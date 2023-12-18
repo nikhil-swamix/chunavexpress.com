1e01efa7adf5ecf95a6458fd4f4545bd978b4e05
M Nikhil Swami <nikhilswami1@gmail.com>
2023-12-18 23:11:25 +0530

    og integration and langfix

--- frontend/src/routes/post/[slug]/+page.svelte
+++ frontend/src/routes/post/[slug]/+page.svelte
@@ -10,9 +10,21 @@
 
 	$: {
 		langpref = $page.url.searchParams.get('lang') == 'en' ? 'english' : 'hindi';
-
 	}
+	console.log($page.url);
+	let body = langpref == 'hindi' ? data.docs[0].body_hindi : data.docs[0].body_english;
+	let description = body.root.children[0].children[0].text
 </script>
+
+<svelte:head>
+	<!-- Replace these values with your specific metadata -->
+	<title>{data.docs[0].title}</title>
+	<meta property="og:title" content={data.docs[0].title} />
+	<meta property="og:description" content="{description}" />
+	<meta property="og:image" content={data.docs[0].meta.image.url} />
+	<meta property="og:url" content="{$page.url.href}" />
+	<meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
 
 <div class=" row mx-0 px-lg-3">
 	<div class="col-12 col-lg-3 pt-3 px-lg-5">
