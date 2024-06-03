a5dbe4c70194f505bb9278d7f29d6ee0b2bde2d9
M Nikhil Swami <nikhilswami1@gmail.com>
2024-06-04 03:56:49 +0530

    fix links

--- frontend/src/components/SinglePost.svelte
+++ frontend/src/components/SinglePost.svelte
@@ -13,6 +13,7 @@
 	let boldnum = (s) => s?.replace(/\d+/g, `<span class="fw-bold">$&</span>`);
 	let lgSpacer = 'px-xxl-3 px-3';
 	let renders = [];
+	console.log(doc);
 
 	if (langpref == 'hindi') {
 		title = doc.title_hindi || doc.meta.title;
@@ -24,43 +25,54 @@
 
 	body?.root?.children?.map((item) => {
 		if (item?.type == 'paragraph') {
-			
-			let sub_children = item.children?.map((leaf) => {
+			if (item.children.length > 1) {
 				let render = '';
-				let isBold = leaf.format == 1 ? 'fw-bold lead' : '';
-				let spacer = 'mb-4 easy-reader';
-				let text = boldnum(leaf.text);
-				if (leaf.type == 'linebreak') {
-					return;
-				}
-	
-				if (item.type === 'paragraph') {
-					render = `<p class="${isBold} ${spacer}">${text}</p>`;
-				} else if (item.type === 'image') {
-					render = `<img src=${item.url} alt=${item.alt} />`;
-				} else if (item.type === 'heading') {
-					let text = spanrenderer(item.children[0].text, 'btn btn-dark px-2 py-0');
-					render = `<${item.tag} class="${spacer} ">${text}</${item.tag}>`;
-				} else if (item.type === 'list') {
-					render = `<${item.tag} class="${spacer}">${item.children[0].text}</${item.tag} style="margin-left: 20px;">`;
-				}
-	
-				renders.push(render);
-			});
-		
-		} else if(item?.type == 'upload') { 
+				item.children?.map((leaf) => {
+					if (leaf.type == 'link') {
+						render += `<a href="${leaf?.fields?.url}" target="_blank">${leaf?.children[0]?.text}</a>`;
+					} else {
+						render += boldnum(leaf.text);
+					}
+					console.log(render);
+				});
+				renders.push(`<p class="easy-reader">${render}</p>`);
+			} else {
+				item.children?.map((leaf) => {
+					let render = '';
+					let isBold = leaf.format == 1 ? 'fw-bold lead' : '';
+					let spacer = ' easy-reader';
+					let text = boldnum(leaf.text);
+					if (leaf.type == 'linebreak') {
+						return 'return';
+					}
+
+					if (item.type === 'paragraph') {
+						render = `<p class="${isBold} ${spacer}">${text}</p>`;
+					} else if (item.type === 'image') {
+						render = `<img src=${item.url} alt=${item.alt} />`;
+					} else if (item.type === 'heading') {
+						let text = spanrenderer(item.children[0].text, 'btn btn-dark px-2 py-0');
+						render = `<${item.tag} class="${spacer} ">${text}</${item.tag}>`;
+					} else if (item.type === 'list') {
+						render = `<${item.tag} class="${spacer}">${item.children[0].text}</${item.tag} style="margin-left: 20px;">`;
+					}
+
+					// console.log(item.type);
+
+					renders.push(render);
+				});
+			}
+		} else if (item?.type == 'upload') {
 			renders.push(`
 			<div class="col-12  px-lg-3 d-flex justify-content-center">
 			<img src="https://chunavexpress.com${item?.value.url}" alt=${item?.value?.alt} />
 			</div>
 			`);
-
-			
 		}
 
 		return renders;
 	});
-	function copy2clipboard () {
+	function copy2clipboard() {
 		navigator.clipboard.writeText(`https://chunavexpress.com/post/${doc.slug}`);
 		alert('Link Copied to Clipboard!');
 	}
@@ -75,8 +87,7 @@
 {#if body}
 	<div class="card border-2 mb-5 mt-lg-3 shadow">
 		<div class="post-head {lgSpacer}">
-			
-			<a href="https://chunavexpress.com/post/{doc.slug}" target="single" type="button" class="" >
+			<a href="https://chunavexpress.com/post/{doc.slug}" target="single" type="button" class="">
 				<h2 class="pt-lg-2 py-2">{@html title}</h2>
 				<!-- <i class="fa-solid fa-arrow-up-right-from-square" /> Open -->
 			</a>
@@ -109,9 +120,8 @@
 	.card img {
 		transform: scale(1.03);
 		min-height: 10em;
-		
 	}
-	h2{
+	h2 {
 		color: #000 !important;
 	}
 </style>
